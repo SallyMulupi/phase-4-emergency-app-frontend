8877476177721842090466186a271f478d2c3d38
SallyMulupi <mulupisally2gmail.com>
2022-10-17 01:05:41 +0300

    add image in about

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavBar from './NavBar'
+const About = () => {
+    return (
+      <div>
+          <NavBar/>
+          <div className="ambulencedescription">
+        <div className="description">
+          <h2>Ambulance</h2>
+          <p>vehicle equipped to provide emergency care to sick or injured people and to get them to hospital.
+             Ambulances can also be used to transport patients between hospitals. There are several types of ambulance</p>
+  
+        </div>
+        <img src="https://media.istockphoto.com/photos/ambulance-van-on-highway-with-flashing-lights-picture-id911803146?s=2048x2048" alt="image"/>
+       </div>
+  
+  
+      </div>
+    )
+  }
+  
+  export default About
